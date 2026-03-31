eb2916f3a436e8a3b9e7163e8ea6a49460faed6b
moertlinger-revived <moertlinger@gmail.com>
2026-03-31 14:01:15 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyD07HOBcrcN3GzgRkWwvO4VX-42MO7YSmI",
-    databaseURL: "https://tablero-full-revived-default-rtdb.firebaseio.com",
-    projectId: "tablero-full-revived",
-    // ... el resto de tus datos
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
